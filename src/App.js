727f5d3cd402bb1f2abf7ca0dea1a49f0f1149ae
Krishna841 <65186847+Krishna841@users.noreply.github.com>
2024-09-19 00:24:19 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import "./App.css";
-import addNotification from "react-push-notification";
 
 function App() {
   const clickNotify = () => {
-    addNotification({
-      title: "SRMD Push Notification",
-      message: "Jai Prabhu",
-      duration: 5000,
-      native: true,
-      onClick: () => {
-        window.open("http://localhost:3000", "_blank"); // Replace with the desired URL
-      },
-    });
-    console.log("mira");
+    if (Notification.permission === "granted") {
+      new Notification("Notification Title", {
+        body: "This is the notification message.",
+        icon: "/path/to/icon.png", // Optional: add an icon
+      }).onclick = () => {
+        window.open("https://yourwebsite.com", "_blank");
+      };
+    } else {
+      console.log("Notification permission denied");
+    }
   };
   return (
     <div className="App">
